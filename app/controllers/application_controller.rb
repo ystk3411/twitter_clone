ee62f4983463f1fd9e464848662c98998c3c554e
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-04-08 23:09:50 +0900

    トップページへのルーティングを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    tweets_index_path
+    tweets_path
   end
 
   def after_sign_out_path_for(resource)
-    tweets_index_path
+    tweets_path
   end
   protected
   def configure_permitted_parameters
