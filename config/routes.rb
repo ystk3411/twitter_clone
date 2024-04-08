dcd640458fb90d9537e64c99a9d7630ee70b379c
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-04-08 22:08:16 +0900

    ルートパスを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'tweets/index'
-  get 'tweets/show'
+  root 'tweets#index'
   devise_for :users
   resources :tasks
-  resource :tweets
+  resources :tweets
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
