d2de7004d15a766d92d035a43616d343b61e6d3d
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-04-28 02:20:52 +0900

    Relationshipsモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_27_134220) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_27_171830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
