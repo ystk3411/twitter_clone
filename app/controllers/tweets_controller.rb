621d83110640b61f3584f7dee5111be1887af6d5
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-05-04 01:22:20 +0900

    タブの切り替えを実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,10 @@
 class TweetsController < ApplicationController
   def index
     tweets = Tweet.all.order(created_at: :desc)
+    tweets_following = Tweet.where(user_id:current_user.id)
     @tweets = Kaminari.paginate_array(tweets).page(params[:page]).per(10)
+    @tweets_following = Kaminari.paginate_array(tweets_following).page(params[:page]).per(10)
+    p tweets_following
   end
 
   def show; end
