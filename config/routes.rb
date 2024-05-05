a11cf28f29a6e73e8a8b92d4c1c091dcecee3f86
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-05-05 20:08:40 +0900

    users_controllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root 'tweets#index'
   devise_for :users,
              controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   resources :tasks
   resources :tweets
+  resources :users
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
