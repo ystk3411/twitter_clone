2ca3d0dfa9834dc3ad96198cb1211db97a18c26f
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-05-27 22:20:39 +0900

    いいね機能を実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class LikesController < ApplicationController
+
+  def create
+    like = current_user.likes.build
+    like.tweet_id = params[:tweet_id]
+    like.save
+    redirect_to request.referer
+  end
+
+  def destroy
+  end
 end
