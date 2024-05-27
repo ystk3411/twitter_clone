e515fc76ab54558cc53ea0c2019500caa6b04a38
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-05-28 01:10:23 +0900

    いいねをしたかの判定メソッドを実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,4 +8,8 @@ class Tweet < ApplicationRecord
   has_many :read_counts, dependent: :destroy
   validates :content, { length: { maximum: 140 } }
   has_one_attached :image
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
