9f8350708c45b40198d285ee48499c39ca336514
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2024-08-05 22:35:15 +0900

    単体テスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with email, password, telephone_number, birth_day and name" do
+    user = User.new(email: "yoshitaka.sample@sample.com",
+                    password: "password",
+                    telephone_number: "0127117117",
+                    birth_day: Date.new(2020, 9, 5),
+                    name: "yoshitaka")
+    
+    expect(user).to be_valid
+  end
+
+  it "is invalid without name" do
+    user = User.new(email: "yoshitaka.sample@sample.com",
+                    password: "password",
+                    telephone_number: "0127117117",
+                    birth_day: Date.new(2020, 9, 5),
+                    name: nil
+                    )
+    
+    expect(user).to be_valid
+  end
+
+  it "is invalid email format" do
+    user = User.new(email: "yoshitaka.sample_sample.com",
+                    password: "password",
+                    telephone_number: "0127117117",
+                    birth_day: Date.new(2020, 9, 5),
+                    name: "yoshitaka"
+                    )
+    
+    expect(user).to be_valid
+  end
+
+  it "is invalid birth_day later than now" do
+    user = User.new(email: "yoshitaka.sample@sample.com",
+                    password: "password",
+                    telephone_number: "0127117117",
+                    birth_day: Date.new(2100, 9, 5),
+                    name: "yoshitaka"
+                    )
+    
+    expect(user).to be_valid
+  end
+
+  it "is invalid email not unique" do
+    user1 = User.create(email: "yoshitaka.sample@sample.com",
+                    password: "password",
+                    telephone_number: "0127117117",
+                    birth_day: Date.new(2020, 9, 5),
+                    name: "yoshitaka"
+                    )
+    
+    user2 = User.new(email: "yoshitaka.sample@sample.com",
+    password: "password",
+    telephone_number: "0127117117",
+    birth_day: Date.new(2020, 9, 5),
+    name: "yoshitaka"
+    )
+    
+    expect(user2).to be_valid
+  end
 end
